e7f6a51b4684aedb524f4c7962015f8afc7fa059
trirumisu <100772401+trirumisu@users.noreply.github.com>
2022-05-08 22:38:47 +0800

    Update gjx.py

--- gjx.py
+++ gjx.py
@@ -53,7 +53,46 @@ def TP_model(nodejs):
     #模型求解
     #初始化求解时间
     prob.solve(cbc)
+def TP_model_1(nodejs):
 
+    aa=os.getcwd()
+
+    import pulp
+    try:
+        prob = pulp.LpProblem("init_sdp",pulp.LpMinimize)#调用pulp
+    except:
+        prob = pulp.LpProblem("init_sdp",const.LpMinimize)#调用pulp
+    variables = {}
+    costs = {}
+
+    for i in range(num_united):
+        for j in range(num_units):
+            variables["x_" +str(i)+ "_"+ str(j)]=pulp.LpVariable("x_" +str(i)+ "_"+ str(j), 0, None, pulp.LpInteger) #
+            cost=nodejs[i][j] 
+            costs["x_" +str(i)+ "_"+ str(j)]=cost
+    obj = 0
+    # 计算所有需求点到所有供给点的总成本
+    for x in variables:
+        obj += costs[x] * variables[x]
+    prob += obj
+    #对于每个需求点来说，被服务的人口之和要等于需求点总人口
+    for i in range(num_united):
+        s = 0
+        for j in range(num_units):
+            s += variables["x_" + str(i) + "_" + str(j)]
+        prob += s == nodes[i]
+    for k in range(num_units):
+        s = 0
+        for i in range(num_united):
+            s += variables["x_" + str(i) + "_" + str(k)]
+        prob += s <= facilityCapacity[k]
+
+
+    cbc = COIN_CMD()
+
+    #模型求解
+    #初始化求解时间
+    prob.solve(cbc)
     if prob.status<0:
         arcpy.AddMessage("错误。求解失败，请重新核对数据")
     else:
